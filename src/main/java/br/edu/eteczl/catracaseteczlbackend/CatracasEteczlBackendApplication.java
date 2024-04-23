44625951c4924e23db3df2e4e60ad7da7729ac3a
jeffersonlds3009 <jeffersonlds3009@gmail.com>
2024-04-23 00:32:23 +0000

    feat: setup and Add commitlint, commitizen and husky config

---
+++ src/main/java/br/edu/eteczl/catracaseteczlbackend/CatracasEteczlBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.eteczl.catracaseteczlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatracasEteczlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatracasEteczlBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/eteczl/catracaseteczlbackend/CatracasEteczlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.eteczl.catracaseteczlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatracasEteczlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
